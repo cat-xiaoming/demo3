0a9842db857efc3d75f3a7ef446b2721c5214857
msg <934722253@qq.com>
2023-02-03 17:05:30 +0800

    dev 2

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -9,5 +9,8 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("ok");
+        System.out.println("dev1");
+        System.out.println("dev2");
+
     }
 }
